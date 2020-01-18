bb0455417bd3c9de424e2e078c4d838d0b9b5733
avizmarques <aviz.marques@gmail.com>
2020-01-18 17:11:51 +0100

    improved code on assignment 2

--- src/transform.js
+++ src/transform.js
@@ -1,41 +1,37 @@
+function addToObj(obj, group, people) {
+  if(people.length) {
+    obj[group] = people;
+  }
+}
+
 function groupAdultsByAgeRange(people) {
   const groups = ['20 and younger', '21-30', '31-40', '41-50', '51 and older'];
   
   return groups.reduce((obj, group) => {
     switch (group) {
       case '20 and younger':
-        let youngest = people.filter(person => person.age >= 18 && person.age <= 20);
-        if (youngest.length) {
-          obj[group] = youngest;
-        }
+        const youngest = people.filter(person => person.age >= 18 && person.age <= 20);
+        addToObj(obj, group, youngest);
         break;
 
       case '21-30':
-        let younger = people.filter(person => person.age > 20 && person.age <= 30);
-        if(younger.length) {
-          obj[group] = younger;
-        }
+        const younger = people.filter(person => person.age > 20 && person.age <= 30);
+        addToObj(obj, group, younger);
         break;
 
       case '31-40':
-        let young = people.filter(person => person.age > 30 && person.age <= 40);
-        if(young.length) {
-          obj[group] = young;
-        }
+        const young = people.filter(person => person.age > 30 && person.age <= 40);
+        addToObj(obj, group, young);
         break;
 
       case '41-50':
-        let older = people.filter(person => person.age > 40 && person.age <= 50);
-        if(older.length) {
-          obj[group] = older;
-        }
+        const older = people.filter(person => person.age > 40 && person.age <= 50);
+        addToObj(obj, group, older);
         break;
 
       case '51 and older':
-        let oldest = people.filter(person => person.age > 50);
-        if(oldest.length) {
-          obj[group] = oldest;
-        }
+        const oldest = people.filter(person => person.age > 50);
+        addToObj(obj, group, oldest);
         break;
     } 
     return obj;
