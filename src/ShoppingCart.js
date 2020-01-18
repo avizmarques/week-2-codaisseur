c0a38d1be16cb44965352de7d5447059f2723234
avizmarques <aviz.marques@gmail.com>
2020-01-18 12:01:52 +0100

    first assignment completed

---
+++ src/ShoppingCart.js
@@ -0,0 +1,25 @@
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  addItem(itemName, quantity, price) {
+    this.items.push( { name: itemName, quantity: quantity, pricePerUnit: price } );
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  total() {
+    return this.items.reduce((total, item) => {
+      return total + ( item.quantity * item.pricePerUnit ); 
+    }, 0);
+  }
+}
+
+module.exports = ShoppingCart;
